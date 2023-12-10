8227c6e5dc9c55d3fb3c366257a30c458d5c0a3f
jobayerhasansakib <bsakibmd11794@gmail.com>
2023-12-11 03:34:50 +0600

    added navbar

---
+++ src/Components/Navbar/Navlink.jsx
@@ -0,0 +1,19 @@
+
+
+const NavLink = () => {
+    return (
+        <div className="flex-none hidden lg:block">
+            <ul className="menu menu-horizontal">
+                <li><a>Home</a></li>
+                <li><a>Skills</a></li>
+                <li><a>Experience</a></li>
+                <li><a>Projects</a></li>
+                <li><a>Education</a></li>
+                <li><a>Contact me</a></li>
+
+            </ul>
+        </div>
+    );
+};
+
+export default NavLink;
---
+++ src/Components/Navbar/Sidebar.jsx
@@ -0,0 +1,16 @@
+
+const Sidebar = () => {
+    return (
+        <ul className="menu p-4 w-80 min-h-full bg-base-200">
+            {/* Sidebar content here */}
+            <li><a>Home</a></li>
+            <li><a>Skills</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Education</a></li>
+            <li><a>Contact me</a></li>
+        </ul>
+    );
+};
+
+export default Sidebar;
