bb5a4d919ed80abe69d2905e5c7a4cb459be49ec
jobayerhasansakib <bsakibmd11794@gmail.com>
2023-12-11 22:30:05 +0600

    added all the skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import Lottie from "lottie-react";
-import { FaHtml5, FaCss3, FaReact } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { SiMongodb, SiTailwindcss } from "react-icons/si";
+import { FaHtml5, FaCss3, FaReact, FaNode, FaGithub, FaGitAlt } from "react-icons/fa";
+import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
+import { SiExpress, SiMongodb, SiNetlify, SiTailwindcss } from "react-icons/si";
 import skill from "../../assets/skills.json"
 
 const Skills = () => {
@@ -13,9 +13,9 @@ const Skills = () => {
                     <Lottie className="w-96 h-96" animationData={skill}></Lottie>
                 </div>
                 <div>
-                    <h3 className="text-3xl">Here are some skill that I am proficient with </h3>
+                    <h3 className="text-xl lg:text-3xl">Here are some skill that I am proficient with </h3>
                     <div>
-                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 text-center">
+                        <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-2 text-center">
                             <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
                                 <FaHtml5 className="text-6xl text-red-500"></FaHtml5>
                                 <p className="text-center mt-3">HTML</p>
@@ -39,6 +39,30 @@ const Skills = () => {
                             <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
                                 <SiMongodb className="text-6xl text-green-700" />
                                 <p className="text-center mt-3">MongoDb</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <FaNode className="text-6xl text-green-700" />
+                                <p className="text-center mt-3">Node</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <SiExpress className="text-6xl text-black" />
+                                <p className="text-center mt-3">Express</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <IoLogoFirebase className="text-6xl text-yellow-400" />
+                                <p className="text-center mt-3">Firebase</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <FaGithub className="text-6xl text-black" />
+                                <p className="text-center mt-3">Github</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <FaGitAlt className="text-6xl text-red-500" />
+                                <p className="text-center mt-3">Git</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-md shadow-xl p-5 mx-auto my-3">
+                                <SiNetlify className="text-6xl text-green-500" />
+                                <p className="text-center mt-3">Git</p>
                             </div>
                         </div>
                     </div>
